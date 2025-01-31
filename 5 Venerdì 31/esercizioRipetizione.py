741e89675bc6840535d18a4a7770b7da2445bb5b
MarioSaccone <mariosaccone007@gmail.com>
2025-01-31 16:44:20 +0100

    Add files via upload

--- 5 Venerdì 31/esercizioRipetizione.py
+++ 5 Venerdì 31/esercizioRipetizione.py
@@ -1,18 +1,32 @@
+#definire le operazioni
 def addizione(numero1, numero2):
-        return numero1 + numero2 
+    return numero1 + numero2 
+
+def sottrazione(numero1, numero2):
+    return numero1 - numero2
+
+def moltiplicazione(numero1, numero2):
+    return numero1 * numero2
+
+def divisione(numero1, numero2):
+    if numero2 != 0:
+        return numero1 / numero2
+    else:
+        return "Errore: divisione per zero!"
+
 
 lista_risultati = []
 
 x = int(input("Quanti calcoli vuoi fare? "))
+
 for i in range(x):
-
-
     numero1 = float(input("Inserisci il primo numero: "))
     numero2 = float(input("Inserisci il secondo numero "))
 
 #chiedere all'utente quante volte vuole fare un calcolo
     while x <= 0:
-        print("Per favore ripeti, inserisci un valor valido")
+        print("Per favore ripeti, inserisci un valore valido")
+        x = int(input("Quanti calcoli vuoi fare? "))
 
 #chiedere all'utente che tipo di operazione vuole fare attraverso un menù
     print("Che operazione vuoi fare?")
@@ -20,10 +34,26 @@ for i in range(x):
     print("-")
     print("*")
     print("/")
-    scelta = input("+ - * / ")
+    print("Esci")
+    
+    scelta = input("+ - * / oppure Esci ")
 
     if scelta == "+":
        risultato = addizione(numero1, numero2)
-       lista_risultati.append(risultato)
+    elif scelta == "-":
+        risultato = sottrazione(numero1, numero2)
+    elif scelta == "*":
+        risultato = moltiplicazione(numero1, numero2)
+    elif scelta == "/":
+        risultato = divisione(numero1, numero2)
+    else:
+        ("Esci")
+
+    
+#stampa dei risultati
+    lista_risultati.append(risultato)
+    
+for pippo in lista_risultati:
+    print(pippo)
 
     
